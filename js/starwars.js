00dccdcfe08597a86bb3be5625183ec558a53ebd
michellewert7 <mpwert@gmail.com>
2019-10-31 16:10:57 -0600

    importing assets and implementing buttons

---
+++ js/starwars.js
@@ -0,0 +1,74 @@
+import { films } from '../assets/films.js'
+import { people } from '../assets/people.js'
+
+console.log('Hey, I am JavaScript on your page!')
+
+let mainArea = document.querySelector('main')
+let mainHeader = document.querySelector('header')
+
+/* films.forEach(function(film){
+    let filmDiv = document.createElement('div')
+    let title = document.createElement('h1')
+    let crawl = document.createElement('p') 
+
+    filmDiv.appendChild(title)
+    filmDiv.appendChild(crawl)
+
+    title.textContent = film.title
+    crawl.innerText = film.opening_crawl
+
+    mainArea.appendChild(filmDiv)
+}); */
+
+const maleCharacters = people.filter (person => person.gender === 'male')
+const femaleCharacters = people.filter (person => person.gender === 'female')
+
+let maleButton = document.createElement('button')
+maleButton.textContent = "Male Characters"
+maleButton.addEventListener('click', event => {
+    maleCharacters.forEach(elt =>{
+        console.log(elt)
+        //elt.setAttribute("style", "visibility: visible;")
+    })
+    femaleCharacters.forEach(elt=> {
+        elt.setAttribute("style", "visibility: hidden;")
+    })
+})
+let femaleButton = document.createElement('button')
+femaleButton.textContent = "Female Characters"
+femaleButton.addEventListener('click', event => {
+
+})
+mainHeader.appendChild(maleButton)
+mainHeader.appendChild(femaleButton)
+
+people.forEach(function(person){
+    let personDiv = document.createElement ('div')
+    let name = document.createElement('h1')
+    let gender = document.createElement('p')
+    let pic = document.createElement('img')
+
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+
+    let charNum = getCharNumber(person.url)
+
+    name.textContent = person.name
+    gender.textContent = person.gender
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+
+    mainArea.appendChild(personDiv)
+})
+
+function getCharNumber(charURL) {
+    let end = charURL.lastIndexOf('/')
+    let charID = charURL.substring(end -2, end)
+    if(charID.indexOf('/') !== -1) {
+        return charID.slice(1,2)
+    } else {
+        return charID
+    }
+}
+
+ 
