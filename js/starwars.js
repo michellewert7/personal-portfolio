ae54b9e9dc22d1ed36f7d8ad28b94408c08ebb7a
michellewert7 <mpwert@gmail.com>
2019-11-05 17:05:06 -0700

    gender filtering

--- js/starwars.js
+++ js/starwars.js
@@ -68,7 +68,7 @@ maleButton.addEventListener('click', event => {
 let femaleButton = document.createElement('button')
 femaleButton.textContent = "Female Characters"
 femaleButton.addEventListener('click', event => {
-    femaleCharacters.forEach(elt=> {
+    maleCharacters.forEach(elt=> {
         let matchedDiv = allDivs.filter(element => {
             return element.firstChild.textContent === elt.name
     })
